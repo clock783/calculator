c9715001519657f7c1cee063e53ed0659cd5bfd4
clock783 <aodunlade3@gmail.com>
2024-01-16 08:43:14 -0500

    Create files. Create arithmetic functions and solver func

---
+++ javascript.js
@@ -0,0 +1,20 @@
+//global variables
+let operand1;
+let operand2;
+let operator;
+
+//create function to solve arithmetic with two operands and one opertor
+function solve(a, operator, b){
+    let operation = {
+        '+': add(a,b),
+        '-': subtract(a,b),
+        '*': multiply(a,b),
+        '/': divide(a,b)
+    };
+    return operation[operator];
+}
+
+const add = (a,b) => a + b;
+const subtract = (a,b) => a - b;
+const multiply = (a,b) => a * b;
+const divide = (a,b) => a / b;
