b60c72462fcb1ff41f3266721db563e148661e28
clock783 <aodunlade3@gmail.com>
2024-02-05 07:28:56 -0500

    Setup number buttons to display on screen
    - Reduced code duplication by using array of objects.

--- javascript.js
+++ javascript.js
@@ -43,6 +43,30 @@ const eight = document.getElementById('eight');
 const nine = document.getElementById('nine');
 const zero = document.getElementById('zero');
 
+const numbers = [
+    {element:one, number: 1},
+    {element:two, number: 2},
+    {element:three, number: 3},
+    {element:four, number: 4},
+    {element:five, number: 5},
+    {element:six, number: 6},
+    {element:seven, number: 7},
+    {element:eight, number: 8},
+    {element:nine, number: 9},
+    {element:zero, number: 0}
+];
+
+console.log(numbers[0]['element']);
+//add events to numbers
+for (let num of numbers){
+    // console.log(numbers[num], typeof num);
+    // console.log(num['element']);
+    num['element'].addEventListener('click', function(){
+        bottomLine += num['number'];
+        updateDisplay();
+    });
+};
+
 function updateDisplay(){
     document.getElementById('topLine').textContent = topLine;
     document.getElementById('bottomLine').textContent = bottomLine;
